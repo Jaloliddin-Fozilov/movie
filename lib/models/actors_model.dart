8e5bb97b058a54ba23ecb8da261d84748cee8394
Fozilov-Jaloliddin <jaloliddinish@gmail.com>
2021-11-30 10:28:53 +0500

    models

---
+++ lib/models/actors_model.dart
@@ -0,0 +1,83 @@
+class ActorsModel {
+  final String id;
+  final String name;
+  final String image;
+
+  ActorsModel({
+    required this.id,
+    required this.name,
+    required this.image,
+  });
+}
+
+class Actors {
+  List<ActorsModel> _list = [
+    ActorsModel(
+      id: "a1",
+      name: "Shah Rukh Khan",
+      image:
+          "https://m.media-amazon.com/images/M/MV5BZDk1ZmU0NGYtMzQ2Yi00N2NjLTkyNWEtZWE2NTU4NTJiZGUzXkEyXkFqcGdeQXVyMTExNDQ2MTI@._V1_UY317_CR4,0,214,317_AL_.jpg",
+    ),
+    ActorsModel(
+      id: "a2",
+      name: "Anushka Sharma",
+      image:
+          "https://m.media-amazon.com/images/M/MV5BNGM5YmYwNjUtNzQyYS00OGY0LTljZDAtMDNiMjhiOTFiMzRiXkEyXkFqcGdeQXVyMTExNDQ2MTI@._V1_UY317_CR3,0,214,317_AL_.jpg",
+    ),
+    ActorsModel(
+      id: "a3",
+      name: "Katrina Kaif",
+      image:
+          "https://m.media-amazon.com/images/M/MV5BMjI0Nzc4OTE0NV5BMl5BanBnXkFtZTgwOTczODgyNDM@._V1_UY317_CR10,0,214,317_AL_.jpg",
+    ),
+    ActorsModel(
+      id: "a4",
+      name: "Mohd. Zeeshan Ayyub",
+      image:
+          "https://m.media-amazon.com/images/M/MV5BODcyY2JlZTktYTU4ZS00MGY1LTgyY2MtODA4NWUyZDBlNjc5L2ltYWdlXkEyXkFqcGdeQXVyMTExNDQ2MTI@._V1_UY317_CR12,0,214,317_AL_.jpg",
+    ),
+    ActorsModel(
+      id: "a5",
+      name: "Joaquin Phoenix",
+      image:
+          "https://m.media-amazon.com/images/M/MV5BZGMyY2Q4NTEtMWVkZS00NzcwLTkzNmQtYzBlMWZhZGNhMDhkXkEyXkFqcGdeQXVyNjk1MjYyNTA@._V1_UX214_CR0,0,214,317_AL_.jpg",
+    ),
+    ActorsModel(
+      id: "a6",
+      name: "Frances Conroy",
+      image:
+          "https://m.media-amazon.com/images/M/MV5BMTM4OTIzMTg5MF5BMl5BanBnXkFtZTYwNjA5OTIz._V1_UY317_CR1,0,214,317_AL_.jpg",
+    ),
+    ActorsModel(
+      id: "a7",
+      name: "Robert De Niro",
+      image:
+          "https://m.media-amazon.com/images/M/MV5BMjAwNDU3MzcyOV5BMl5BanBnXkFtZTcwMjc0MTIxMw@@._V1_UY317_CR13,0,214,317_AL_.jpg",
+    ),
+    ActorsModel(
+      id: "a8",
+      name: "Dilorom Karimova",
+      image:
+          "http://cdn.beta.qalampir.uz/uploads/wQ/f_VuMP6fbt5D4qwId8Nk2YQ4LbreUUxH.jpg",
+    ),
+    ActorsModel(
+      id: "a9",
+      name: "Fotih Nasimov",
+      image: "https://www.kino-teatr.ru/acter/foto/post/315699.jpg",
+    ),
+    ActorsModel(
+      id: "a10",
+      name: "Husan Rashidov",
+      image: "https://i.imgur.com/Oc7mHjp.png",
+    ),
+    ActorsModel(
+      id: "a11",
+      name: "Rano Zakirova",
+      image: "https://i.ytimg.com/vi/uCn7LX4X4RU/maxresdefault.jpg",
+    ),
+  ];
+
+  List<ActorsModel> get list {
+    return _list;
+  }
+}
--- lib/models/movies_model.dart
+++ lib/models/movies_model.dart
@@ -3,6 +3,7 @@ class MoviesModel {
   final String categoryId;
   final String title;
   final List<String> imageUrls;
+  final List<String> actors;
   final String description;
   final int star;
 
@@ -11,6 +12,7 @@ class MoviesModel {
     required this.categoryId,
     required this.title,
     required this.imageUrls,
+    required this.actors,
     required this.description,
     required this.star,
   });
@@ -25,24 +27,133 @@ class Movies {
       imageUrls: [
         "https://avatars.mds.yandex.net/get-kinopoisk-image/1629390/3c8f0854-cab4-40f7-8086-df7249ac903a/600x900",
         "https://m.media-amazon.com/images/M/MV5BMzg0NjU3MzgtZGFhMC00YjQ4LWE3YWUtNWRlNWJhYzhjNmQ1XkEyXkFqcGdeQXVyNTYwMzA0MTM@._V1_.jpg",
-        "https://i.ytimg.com/vi/INlFMT09hlI/maxresdefault.jpg"
+        "https://i.ytimg.com/vi/INlFMT09hlI/maxresdefault.jpg",
       ],
+      actors: ["a1", "a2", "a3", "a4"],
       description:
           "Zero is a 2018 Indian romantic comedy-drama film written by Himanshu Sharma, directed by Aanand L. Rai and produced by Colour Yellow Productions in collaboration with Red Chillies Entertainment. Starring Shah Rukh Khan, Katrina Kaif and Anushka Sharma in leading roles, Zero revolves around the most prominent character Bauua Singh, a short man from Meerut who after having difficulty finding a marriage partner, finds a companion in Aafia Bhinder, a NSAR (a fictional space research facility) scientist with cerebral palsy. However, film superstar Babita also gets close with him, testing his first relationship, before this love triangle takes them to far-off cities, thrusting Bauua into an adventure to discover both his true love and completeness in a life lived to the fullest.",
       star: 4,
     ),
     MoviesModel(
-      id: "m3",
+      id: "m2",
       categoryId: "c1",
       title: "Barfi!",
       imageUrls: [
-        "https://upload.wikimedia.org/wikipedia/en/thumb/2/2e/Barfi%21_poster.jpg/220px-Barfi%21_poster.jpg"
-            "https://i.ytimg.com/vi/TcDmarWK68c/maxresdefault.jpg",
+        "https://upload.wikimedia.org/wikipedia/en/thumb/2/2e/Barfi%21_poster.jpg/220px-Barfi%21_poster.jpg",
+        "https://i.ytimg.com/vi/TcDmarWK68c/maxresdefault.jpg",
         "https://images.mygoodtimes.in/wp-content/uploads/2019/09/12130834/Barfi-e1568293729597.jpg",
       ],
+      actors: ["a1", "a2", "a3", "a4"],
       description:
           "Barfi! is a 2012 Indian Hindi-language romantic comedy-drama film written and directed by Anurag Basu and produced by Ronnie Screwvala and Siddharth Roy Kapur under UTV Motion Pictures. Set in the 1970s, the film depicts the story of Murphy 'Barfi' Johnson (a deaf-mute boy from Darjeeling) and his relationships with two women, Shruti and Jhilmil (who is autistic). The film stars Ranbir Kapoor, Priyanka Chopra, and Ileana D'Cruz in the lead roles, with Saurabh Shukla, Ashish Vidyarthi, Jisshu Sengupta and Roopa Ganguly in supporting roles.",
       star: 5,
+    ),
+    MoviesModel(
+      id: "m3",
+      categoryId: "c1",
+      title: "Devdas",
+      imageUrls: [
+        "https://avatars.mds.yandex.net/get-kinopoisk-image/1777765/e574f6ae-4be2-4ab4-bdbc-89ff6c39c4b0/300x450",
+        "https://avatars.mds.yandex.net/get-kinopoisk-image/1946459/f4776449-f5ff-43c4-8602-e60c80487b4d/x252",
+        "https://avatars.mds.yandex.net/get-kinopoisk-image/1900788/20ac3546-359b-44bd-85d6-27e9ce7ac3da/2560x",
+      ],
+      actors: ["a1", "a2", "a3", "a4"],
+      description:
+          "Devdas is a Bengali-language film based on the Sharat Chandra Chattopadhyay novel Devdas. It is the first Bangladeshi version of the story and the first colour film version in Bangladesh. It was the first of two versions directed by Chashi Nazrul Islam.",
+      star: 45,
+    ),
+    MoviesModel(
+      id: "m4",
+      categoryId: "c1",
+      title: "My Name Is Khan",
+      imageUrls: [
+        "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/5Y36lCiNyyV71mjq6LavgiggbhT.jpg",
+        "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/xuqgovav0bbzwxzSg9K0RcEzLPj.jpg",
+      ],
+      actors: ["a1", "a2", "a3", "a4"],
+      description:
+          "Rizwan Khan, a Muslim from the Borivali section of Mumbai, has Asperger's syndrome. He marries a Hindu single mother, Mandira, in San Francisco. After 9/11, Rizwan is detained by authorities at LAX who treat him as a terrorist because of his condition and his race.",
+      star: 3,
+    ),
+    MoviesModel(
+      id: "m5",
+      categoryId: "c2",
+      title: "Joker",
+      imageUrls: [
+        "https://www.themoviedb.org/t/p/w220_and_h330_face/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg",
+        "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/n6bUvigpRFqSwmPp1m2YADdbRBc.jpg",
+        "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/f5F4cRhQdUbyVbB5lTNCwUzD6BP.jpg",
+      ],
+      actors: ["a5", "a6", "a7"],
+      description:
+          "During the 1980s, a failed stand-up comedian is driven insane and turns to a life of crime and chaos in Gotham City while becoming an infamous psychopathic crime figure.",
+      star: 5,
+    ),
+    MoviesModel(
+      id: "m6",
+      categoryId: "c2",
+      title: "Once Upon a Time… in Hollywood ",
+      imageUrls: [
+        "https://www.themoviedb.org/t/p/w220_and_h330_face/8j58iEBw9pOXFD2L0nt0ZXeHviB.jpg",
+        "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/hj59t2wo5sYdPW3OSONMHYmAz09.jpg",
+        "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/oRiUKwDpcqDdoLwPoA4FIRh3hqY.jpg",
+      ],
+      actors: ["a5", "a6", "a7"],
+      description:
+          "Los Angeles, 1969. TV star Rick Dalton, a struggling actor specializing in westerns, and stuntman Cliff Booth, his best friend, try to survive in a constantly changing movie industry. Dalton is the neighbor of the young and promising actress and model Sharon Tate, who has just married the prestigious Polish director Roman Polanski…",
+      star: 4,
+    ),
+    MoviesModel(
+      id: "m7",
+      categoryId: "c2",
+      title: "Ad Astra",
+      imageUrls: [
+        "https://www.themoviedb.org/t/p/w220_and_h330_face/xBHvZcjRiWyobQ9kxBhO6B2dtRI.jpg",
+        "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/AeDS2MKGFy6QcjgWbJBde0Ga6Hd.jpg",
+        "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/cjytKnwXW8Gy1HA6Ejwso4FmPSv.jpg",
+      ],
+      actors: ["a5", "a6", "a7"],
+      description:
+          "The near future, a time when both hope and hardships drive humanity to look to the stars and beyond. While a mysterious phenomenon menaces to destroy life on planet Earth, astronaut Roy McBride undertakes a mission across the immensity of space and its many perils to uncover the truth about a lost expedition that decades before boldly faced emptiness and silence in search of the unknown.",
+      star: 3,
+    ),
+    MoviesModel(
+      id: "m8",
+      categoryId: "c3",
+      title: "Ilhaq",
+      imageUrls: [
+        "https://tasix.online/uploads/posts/2020-05/1588930442_bezymjannyj.png",
+        "https://tasix.online/uploads/posts/2020-05/thumbs/1588930093_ikkinchi_zha_on_urushi_alabasining_75_yilligiga_ba_ishlangan1.jpg",
+        "https://tasix.online/uploads/posts/2020-05/thumbs/1588930056_img_4702-xl1.jpg",
+      ],
+      actors: ["a8", "a9", "a10", "a11"],
+      description: "Uzbek film",
+      star: 5,
+    ),
+    MoviesModel(
+      id: "m9",
+      categoryId: "c3",
+      title: "Temur",
+      imageUrls: [
+        "https://tasix.online/uploads/posts/2020-05/1588919386_12116u20453_t_v1.jpeg",
+        "https://i.ytimg.com/vi/uCL9l0oeW04/maxresdefault.jpg",
+      ],
+      actors: ["a8", "a9", "a10", "a11"],
+      description: "Uzbek film",
+      star: 4,
+    ),
+    MoviesModel(
+      id: "m10",
+      categoryId: "c3",
+      title: "Panoh",
+      imageUrls: [
+        "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/hd8dqOW7urnznHjjnvpaGMvlSJx.jpg",
+        "https://i.ytimg.com/vi/8dxJb9RVJas/maxresdefault.jpg",
+        "https://vdp.mycdn.me/getImage?id=343454583393&idx=14&thumbType=32",
+      ],
+      actors: ["a8", "a9", "a10", "a11"],
+      description: "Uzbek film",
+      star: 4,
     ),
   ];
 
